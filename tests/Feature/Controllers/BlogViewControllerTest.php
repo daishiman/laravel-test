86e8b59a72e56597567a94cdba0401816e65a371
daishiman <manju.manju.03.28@gmail.com>
2022-02-17 18:28:07 +0900

    feat: ブログのタイトルの一覧表示

--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,17 +2,26 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test index */
     public function ブログのTOPページを開ける()
     {
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
         $response = $this->get('/');
 
         $response->assertOk();
+        $response->assertSee($blog1->title);
+        $response->assertSee($blog2->title);
+        $response->assertSee($blog3->title);
     }
 }
