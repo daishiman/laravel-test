e3a345d6b71623d65050e90875bcdad40fe4a339
daishiman <manju.manju.03.28@gmail.com>
2022-02-18 13:57:12 +0900

    feat: 別ディレクトリからの以降

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title'   => $this->faker->realText(20),
+            'body'    => $this->faker->realText(100),
+        ];
+    }
+}
--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,17 +2,48 @@
 
 namespace Tests\Feature\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test index */
     public function ブログのTOPページを開ける()
     {
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
         $response = $this->get('/');
 
         $response->assertOk();
+        $response->assertViewIs('index');
+        $response->assertSee($blog1->title);
+        $response->assertSee($blog2->title);
+        $response->assertSee($blog3->title);
+
+        // Blog::factory()->create(['title' => 'abcde']);
+        // Blog::factory()->create(['title' => 'fghij']);
+        // Blog::factory()->create(['title' => 'klmno']);
+
+        // $response = $this->get('/');
+        // $response->assertOk();
+        // $response->assertSee('abcde');
+        // $response->assertSee('fghij');
+        // $response->assertSee('klmno');
+    }
+
+    /** factory の観察  */
+    public function factoryの観察()
+    {
+        $blog = Blog::factory()->create();
+
+        var_dump($blog->toArray());
+
+        var_dump(User::get()->toArray());
     }
 }
