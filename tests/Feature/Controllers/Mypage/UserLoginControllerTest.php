7453b70d4f90dab87b5ca2588e5b3f1f51d1c082
daishiman <daishimanju@gmail.com>
2022-03-02 15:16:39 +0900

    feat: ログイン処理（入力チェック）

--- tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
+++ tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
@@ -19,4 +19,23 @@ class UserLoginControllerTest extends TestCase
         $this->get('mypage/login')
             ->assertOk();
     }
+
+    /**
+     * @test login
+     */
+    public function ログイン時の入力チェック()
+    {
+        $url = 'mypage/login';
+
+        $this->from($url)
+            ->post($url, [])
+            ->assertRedirect($url);
+
+        app()->setlocale('testing');
+
+        $this->post($url, ['email' => ''])->assertSessionHasErrors(['email' => 'required']);
+        $this->post($url, ['email' => 'aaa@test@.com'])->assertSessionHasErrors(['email' => 'email']);
+        $this->post($url, ['email' => 'aaa@あああ@.com'])->assertSessionHasErrors(['email' => 'email']);
+        $this->post($url, ['password' => ''])->assertSessionHasErrors(['password' => 'required']);
+    }
 }
