abe94665884e438f8ff4bd915979c1fefb208e42
daishiman <daishimanju@gmail.com>
2022-03-02 16:20:39 +0900

    feat: ログイン処理（ログイン）

--- app/Http/Controllers/Mypage/UserLoginController.php
+++ app/Http/Controllers/Mypage/UserLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Mypage;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 
 class UserLoginController extends Controller
@@ -14,9 +15,15 @@ class UserLoginController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'email'    => ['required', 'email:filter'],
             'password' => ['required'],
         ]);
+
+        if (!auth()->attempt($data)) {
+            throw ValidationException::withMessages(['email' => 'メールアドレスかパスワードが間違っています。']);
+        }
+
+        return redirect('mypage/blogs');
     }
 }
--- tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
+++ tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Feature\Controllers\Mypage;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 /**
  * @see App\Http\Controllers\Mypage\UserLoginController
  */
 class UserLoginControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test index
      */
@@ -37,5 +40,93 @@ class UserLoginControllerTest extends TestCase
         $this->post($url, ['email' => 'aaa@test@.com'])->assertSessionHasErrors(['email' => 'email']);
         $this->post($url, ['email' => 'aaa@あああ@.com'])->assertSessionHasErrors(['email' => 'email']);
         $this->post($url, ['password' => ''])->assertSessionHasErrors(['password' => 'required']);
+    }
+
+    /**
+     * @test login
+     */
+    public function ログインできる()
+    {
+        $postData = [
+            'email' => 'aaa@test.com',
+            'password' => 'abce1234',
+        ];
+
+        $dbData = [
+            'email' => 'aaa@test.com',
+            'password' => bcrypt('abce1234'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $this->post('mypage/login', $postData)
+            ->assertRedirect('mypage/blogs');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /**
+     * @test login
+     */
+    public function IDを間違えているのでログインできない()
+    {
+        $postData = [
+            'email' => 'aaa@bbb.net',
+            'password' => 'abcd1234',
+        ];
+
+        $dbData = [
+            'email' => 'ccc@bbb.net',
+            'password' => bcrypt('abcd1234'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $url = 'mypage/login';
+
+        $this->from($url)
+            ->post($url, $postData)
+            ->assertRedirect($url);
+
+        $this->get($url)
+            ->assertSee('メールアドレスかパスワードが間違っています。');
+
+        $this->from($url)
+            ->followingRedirects()
+            ->post($url, $postData)
+            ->assertSee('メールアドレスかパスワードが間違っています。')
+            ->assertSee('<h1>ログイン画面</h1>', false);
+    }
+
+    /**
+     * @test login
+     */
+    public function パスワードを間違えているのでログインできない()
+    {
+        $postData = [
+            'email' => 'aaa@bbb.net',
+            'password' => 'abcd1234',
+        ];
+
+        $dbData = [
+            'email' => 'aaa@bbb.net',
+            'password' => bcrypt('1234abcd'),
+        ];
+
+        $user = User::factory()->create($dbData);
+
+        $url = 'mypage/login';
+
+        $this->from($url)
+            ->post($url, $postData)
+            ->assertRedirect($url);
+
+        $this->get($url)
+            ->assertSee('メールアドレスかパスワードが間違っています。');
+
+        $this->from($url)
+            ->followingRedirects()
+            ->post($url, $postData)
+            ->assertSee('メールアドレスかパスワードが間違っています。')
+            ->assertSee('<h1>ログイン画面</h1>', false);
     }
 }
